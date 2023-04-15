b317efff51f438581d26ae99991774e351f2047b
dolivare5 <deiverolivares2@gmail.com>
2023-04-14 23:03:34 -0500

    Se optimizó el código de js

--- js/index.js
+++ js/index.js
@@ -7,12 +7,52 @@ const inputEmail = document.getElementById("correo");
 const inputDireccion = document.getElementById("direccion");
 const inputTelefono = document.getElementById("telefono");
 const tabla = document.getElementsByClassName("datos__tabla");
-
-const btnEnviar = document.getElementById("enviar");
-// Se escucha el evento submit del formulario
+/**
+ * Se crea un evento que se dispara cuando se envía el formulario
+ */
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    // Se capturan los valores de los inputs
+    const data = extractData();
+    const validacion = validarCampos(data);
+    validacion ? saveData(data) : undefined;
+});
+
+// Se crea otro evento que cuando se cargue la página se muestren los datos del localStorage
+window.addEventListener("DOMContentLoaded", () => {
+    const datos = getItemsFromLocalStorage();
+    console.log('Datos: ', datos);
+    datos.length > 0 ? mostrarDatos(datos) : upDefaultData();
+});
+
+const saveData = (data) => {
+    insertData(data) ? mostrarDatos([data]) : undefined;
+};
+
+const insertData = (user) => {
+    const users = getItemsFromLocalStorage();
+    const isExistUsers = users.find((dato) => dato.identificacion === user.identificacion);
+    if (isExistUsers){
+        alert("El usuario ya existe");
+        return false;
+    }
+    console.log('Usuario: ', user);
+    users.push(user);
+    setItemsToLocalStorage(users);
+    clearForm();
+    return true;
+}
+
+const validarCampos = ({ identificacion, nombre, apellido, email, direccion, telefono }) => {
+    // Se valida que los campos no estén vacíos
+    if (identificacion === "" || nombre === "" || apellido === "" || email === "" || direccion === "" || telefono === "") {
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    return true;
+}
+
+const extractData = () => {
+    // Se extraen los datos del formulario
     const identificacion = inputIdentificacion.value;
     const nombre = inputNombre.value;
     const apellido = inputApellido.value;
@@ -20,96 +60,78 @@ form.addEventListener("submit", (e) => {
     const direccion = inputDireccion.value;
     const telefono = inputTelefono.value;
 
-    if (identificacion === "" || nombre === "" || apellido === "" || email === "" || direccion === "" || telefono === "") {
-        alert("Todos los campos son obligatorios");
-        return;
+    // se retorna un objeto con los datos
+    return {
+        identificacion,
+        nombre,
+        apellido,
+        email,
+        direccion,
+        telefono,
+    }
+}
+
+const getItemsFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem("datos")) || [];
+}
+
+const setItemsToLocalStorage = (data) => {
+    console.log('Data: ', data)
+    localStorage.setItem("datos", JSON.stringify(data));
+}
+
+const upDefaultData = () => {
+    const data1 = {
+        identificacion: "123456789",
+        nombre: "Juan",
+        apellido: "Perez",
+        email: "juan87@gmail.com",
+        direccion: "Calle 1",
+        telefono: "123456789",
+    };
+    const data2 = {
+        identificacion: "987654321",
+        nombre: "Maria",
+        apellido: "Gomez",
+        email: "mari76@gmail.com",
+        direccion: "Calle 2",
+        telefono: "987654321",
     }
 
-    // Se crea un objeto con los datos del formulario
-    const data = { identificacion, nombre, apellido, email, direccion, telefono };
-    // Se muestra en consola el objeto
-    guardarDatos(data);
-});
-
-// Se crea otro evento que cuando se cargue la página se muestren los datos del localStorage
-window.addEventListener("DOMContentLoaded", () => {
-    // Se obtiene el objeto del localStorage
-    const datos = JSON.parse(localStorage.getItem("datos"));
-    console.log(datos);
-    // Se no existen datos se agregan 3 registros y se almacenan en el localStorage
-    if (!datos) {
-        const data1 = {
-            identificacion: "123456789",
-            nombre: "Juan",
-            apellido: "Perez",
-            email: "juan87@gmail.com",
-            direccion: "Calle 1",
-            telefono: "123456789",
-        };
-        const data2 = {
-            identificacion: "987654321",
-            nombre: "Maria",
-            apellido: "Gomez",
-            email: "mari76@gmail.com",
-            direccion: "Calle 2",
-            telefono: "987654321",
-        }
-
-        const data3 = {
-            identificacion: "456789123",
-            nombre: "Pedro",
-            apellido: "Lopez",
-            email: "pedro@gmail.com",
-            direccion: "Calle 3",
-            telefono: "123456789",
-        }
-        guardarDatos(data1);
-        guardarDatos(data2);
-        guardarDatos(data3);
+    const data3 = {
+        identificacion: "456789123",
+        nombre: "Pedro",
+        apellido: "Lopez",
+        email: "pedro@gmail.com",
+        direccion: "Calle 3",
+        telefono: "123456789",
     }
-    if (datos) {
-        console.log(datos);
-        // Se anexa la tabla con los datos del localStorage
-        datos.forEach((data) => {
-            tabla[0].innerHTML += `
-                <tr>
-                    <td>${data.identificacion}</td>
-                    <td>${data.nombre}</td>
-                    <td>${data.apellido}</td>
-                    <td>${data.email}</td>
-                    <td>${data.telefono}</td>
-                    <td>${data.direccion}</td>
-                </tr>
-            `;
-        })
-    }
-});
+    setItemsToLocalStorage([data1, data2, data3]);
+    mostrarDatos([data1, data2, data3]);
+}
 
-const guardarDatos = (data) => {
-    // Se guarda el objeto en el localStorage en formato JSON
-    // Antes de guardar se extrae el objeto del localStorage y se guarda en un array
-    const datos = JSON.parse(localStorage.getItem("datos")) || [];
-    console.log(datos);
-    // Se agrega el nuevo objeto al array
-    // Se verifica que el objeto no exista en el array
-    const existe = datos.find((dato) => dato.identificacion === data.identificacion);
-    if (existe) {
-        alert("El usuario ya existe");
-        return;
-    }
-    datos.push(data);
-    // Se guarda el array en el localStorage
-    localStorage.setItem("datos", JSON.stringify(datos));
+const mostrarDatos = (datos) => {
+    // Se anexa la tabla con los datos que se reciben por parámetro
+    datos.forEach((dato) => {
+        tabla[0].innerHTML += `
+            <tr>
+                <td>${dato.identificacion}</td>
+                <td>${dato.nombre}</td>
+                <td>${dato.apellido}</td>
+                <td>${dato.email}</td>
+                <td>${dato.telefono}</td>
+                <td>${dato.direccion}</td>
+            </tr>
+        `;
+    });
+}
 
-    // Se anexa la tabla con los datos del formulario manteniendo los datos anteriores
-    tabla[0].innerHTML += `
-        <tr>
-            <td>${data.identificacion}</td>
-            <td>${data.nombre}</td>
-            <td>${data.apellido}</td>
-            <td>${data.email}</td>
-            <td>${data.telefono}</td>
-            <td>${data.direccion}</td>
-        </tr>
-    `;
-};
+const clearForm = () => {
+    // Se limpian los campos del formulario
+    inputIdentificacion.value = "";
+    inputNombre.value = "";
+    inputApellido.value = "";
+    inputEmail.value = "";
+    inputDireccion.value = "";
+    inputTelefono.value = "";
+}
